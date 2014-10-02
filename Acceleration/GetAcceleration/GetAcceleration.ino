c59e48948fa71e64212a581c656917bb4c61ade1
Simone Giertz <simone@giertz.com>
2014-10-01 17:15:04 -0700

    Updated comments

--- Acceleration/GetAcceleration/GetAcceleration.ino
+++ Acceleration/GetAcceleration/GetAcceleration.ino
@@ -3,18 +3,21 @@
   
   The acceleration readings are sent over serial and can be accessed in Arduino's Serial Monitor.
   
-  To use the Serial Monitor, set Arduino's serial port to "/tmp/tty.LightBlue-Bean" and the Bean as "Virtual Serial" in the OS X Bean Loader.
+  To use the Serial Monitor, set Arduino's serial port to "/tmp/tty.LightBlue-Bean"
+  and the Bean as "Virtual Serial" in the OS X Bean Loader.
     
   This example code is in the public domain.
 */
 
 void setup() {
-  Serial.begin();   // Bean Serial is at a fixed baud rate. Changing the value in Serial.begin() has no effect.
+  // Bean Serial is at a fixed baud rate. Changing the value in Serial.begin() has no effect.
+  Serial.begin();   
   // Optional: Use Bean.setAccelerationRange() to set the sensitivity to something other than the default of ±2g.
 }
 
 void loop() {
-  AccelerationReading acceleration = Bean.getAcceleration();   // Gets the current acceleration with range of ±2g, and a conversion of 3.91×10-3 g/unit or 0.03834(m/s^2)/units. 
+  // Get the current acceleration with range of ±2g, and a conversion of 3.91×10-3 g/unit or 0.03834(m/s^2)/units. 
+  AccelerationReading acceleration = Bean.getAcceleration();
   
   // Format the serial output like this:    "X: 249  Y: -27   Z: -253"
   String stringToPrint = String();
